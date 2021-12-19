72fb60d461e021c45ce576a0a846af7d7a4688a2
arifdroid <arifdroiddeveloper@gmail.com>
2021-12-19 20:19:02 +0800

    testing map function

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
+import { Map } from './Map'
 
 export default function App() {
     return (
         <div
-        // className='App'
+        // className='demo'
         >
             <div className='header'>
                 <h1>Weather map</h1>
             </div>
+            <div > 
+                <Map />
+            </div>
+            {/* <div>map text</div> */}
+            <div className="footer">(c) 2020 mitsuru ogawa</div>
         </div>
     )
 }
