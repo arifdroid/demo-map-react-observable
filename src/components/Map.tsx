5eedf013cdd8098630a0d9a7072b63615becc40c
arifdroid <arifdroiddeveloper@gmail.com>
2021-12-20 09:50:11 +0800

    add env

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -2,6 +2,7 @@ import React, { useCallback } from "react";
 import { useDispatch } from "react-redux";
 // import { actions } from "../shared/store";
 import '../sytle/Map.css'
+import config from '../config';
 
 declare global {
     interface Window {
@@ -16,7 +17,7 @@ const Map: any = () => {
         if (node !== null) {
             const onLoaded = () => {
                 const map = new google.maps.Map(node, {
-                    center: { lat: 3.074107, lng: 101.492219 }, 
+                    center: { lat: 3.074107, lng: 101.492219 },
                     zoom: 13,
                     mapTypeControl: false,
                     disableDoubleClickZoom: false,
@@ -33,7 +34,7 @@ const Map: any = () => {
             const script = document.createElement("script");
             script.type = "text/javascript";
             script.src =
-                "https://maps.googleapis.com/maps/api/js?key=AIzaSyC5YuDy_h-h-NYXTbw6VLghIaWFym6z98o&callback=initMap";
+                `https://maps.googleapis.com/maps/api/js?key=${config.google_map_api_key}&callback=initMap`;
             script.async = true;
             document.body.appendChild(script);
             window.initMap = onLoaded;
